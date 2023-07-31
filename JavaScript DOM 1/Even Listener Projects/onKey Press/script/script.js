8d7282c08b2beb69c83377d0e9602190781e7bb0
Jai <jaicool4242@gmail.com>
2023-07-31 12:36:32 +0530

    OnKeyPress Project done

---
+++ JavaScript DOM 1/Even Listener Projects/onKey Press/script/script.js
@@ -0,0 +1,15 @@
+// First of all getting all the elements and storing them into varibles
+
+let line1 = document.querySelector(".para1");
+
+let input = document.getElementById("press");
+
+// 
+// 
+//  Adding Event Listener and Arrow Function
+// 
+//  
+
+ function activated(){
+    alert("Woooohoooo! Key is pressed")
+}
