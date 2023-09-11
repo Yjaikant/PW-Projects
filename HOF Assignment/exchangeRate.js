48da4a5ae8539575b20e38c519761656c7d86b69
Jai <jaicool4242@gmail.com>
2023-09-11 18:45:12 +0530

    Exchange of US dollars to INR assignment has been done!

---
+++ HOF Assignment/exchangeRate.js
@@ -0,0 +1,19 @@
+let exchangeRate = 80 ;//DEFINING THE EXCHANGE RATE//
+//CREATING THE CART OBJECT
+let cart = {
+  item1 : 20, 
+  item2 : 30, 
+  item3 : 50, 
+  item4 : 10, 
+  
+}
+// CREATING EMPTY OBJECT FOR CONVERTED PRICES
+let toINR = {};
+// OBJECT.KEYS GIVES AN ARRAY OF ITEMS AND THEN WE USES MAP() METHOD TO ITERATE OVER AN ARRAY. THEN WE STORE THE RESULT AFTER MULTIPLYING BY EXCHANGE RATE WITH THE KEY'S VALUES.
+Object.keys(cart).map((key)=>{
+  toINR[key] = exchangeRate * cart[key];
+});
+
+//PRINTING IT IN CONSOLE
+console.log(toINR)
+// OUTPUT : { item1: 800, item2: 2000, item3: 400 }
