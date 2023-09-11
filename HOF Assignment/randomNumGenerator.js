4ffafe027fab98fe70ad3703091f491a94310817
Jai <jaicool4242@gmail.com>
2023-09-11 15:14:17 +0530

    comments added

--- HOF Assignment/randomNumGenerator.js
+++ HOF Assignment/randomNumGenerator.js
@@ -1,22 +1,24 @@
 
 let delay = 3;
+// Message that displays total seconds in delay //
+console.log(`Delaying for ${delay} Seconds...`)
 
-function countDown(){
+let intervalID = setInterval(countDown,1000);// Creating Interval ID
+
+function countDown(){// defining countDown() function
     if(delay > 0){
         console.log(`Time Remaining to Generate a Randon Number : ${delay} Sec`);
-        delay--;
+        delay--;// decrementing...
     }else{
-        clearInterval(countDownInterval)
+        clearInterval(intervalID)// In clearInterval function we have to add interval ID to it. 
         genRandomNumber();
     }
 }
 
-function genRandomNumber(){
+function genRandomNumber(){// defining genRandomNumber() function
     let randomNumber = Math.floor(Math.random()*100+1);
 
     console.log(`Random Number : ${randomNumber}`);
 }
 
-console.log(`Delaying for ${delay} Seconds...`)
 
-let countDownInterval = setInterval(countDown,1000);
