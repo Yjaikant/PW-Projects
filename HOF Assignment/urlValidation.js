4d16695ad33b9a1b82acc7087771c29260270adf
Jai <jaicool4242@gmail.com>
2023-09-14 10:36:27 +0530

    Regex: URL Validation (Function added)

--- HOF Assignment/urlValidation.js
+++ HOF Assignment/urlValidation.js
@@ -1,13 +1,22 @@
 // JavaScript Code for URL Validation //
+let pattern = /^[A-Za-z]{2,}:\/\/[A-Za-z]{3,}\d{1,}\$.[a-z]{1,}$/
+
+function checkValidation(url){
+    if(pattern.test(url)){
+        return "Input URL's matches the given conditions!";
+    }else{
+        return "Input URL's does not matches the given conditions!";
+    }
+}
 
 let urlCheck1 = "http://thepetnest12$.in";
 let urlCheck2 = "https://thepetnest12$.in";
+let urlCheck3 = "https1://thepetnest12$.in";
+let urlCheck4 = "https://thepetnest12$.in.us";
 
-let pattern = /^[A-Za-z]{2,}:\/\/[A-Za-z]{3,}\d{1,}\$.[a-z]{1,}$/gm
+console.log(checkValidation(urlCheck1));//VALID
+console.log(checkValidation(urlCheck2));//VALID
+console.log(checkValidation(urlCheck3));//INVALID
+console.log(checkValidation(urlCheck4));//INVALID
 
-    if(pattern.test(urlCheck1 && urlCheck2)){
-        console.log("Input URL's matches the given conditions!");
-    }else{
-        console.log("Input URL's does not matches the given conditions!");
-    }
 
