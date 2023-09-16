b7c3b6dd08256580cbbd3da100b5fe5739cb0dd0
Jai <jaicool4242@gmail.com>
2023-09-16 15:14:13 +0530

    Program for Unique elements in set

---
+++ Destructuring/uniqueELementsInSet.js
@@ -0,0 +1,9 @@
+// JavaScript Program for Making a New Set with Unique Elements in it //
+
+// As we know that set only comprises of only unique elements in it. So we will just pass an array inside a new set.
+
+let randomArray = ["a","a","b","a","b","a","b","c","d","d"];
+
+let unique = new Set(randomArray);
+
+console.log(unique);
