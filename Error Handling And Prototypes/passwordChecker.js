7ee54b7d7d49113e5a5257abe3d1b4dec422db0d
Jai <jaicool4242@gmail.com>
2023-09-19 16:55:56 +0530

    JS Program for Password Checker has been completed(getter,setter)

---
+++ Error Handling And Prototypes/passwordChecker.js
@@ -0,0 +1,40 @@
+// JS Program for Passwrod Checker // 
+class User{
+    constructor(username,password){
+        this.username = username;
+        this.password = password;
+    }
+    getPassword(){
+        // In the getPassword method, this line returns the user's password with all characters replaced by asterisks (*). It uses the replace method with a regular expression /./g to replace all characters in the password.//
+        return this.password.replace(/./g,"*");
+        // here g stands for global search!
+    }
+
+    
+
+    setPassword(newPassword){
+        //isValidPassword is a Prototype method of a User class.
+        if(this.isValidPassword(newPassword)){
+            this.password = newPassword;
+        }else{
+            console.log("Error: Password must be at of least 8 characters long and contain at least one number and one uppercase letter");
+        }
+    }
+    isValidPassword(newPassword){
+        let hasNumbers = /[0-9]/g.test(newPassword);
+        let hasUpperCase = /[A-Z]/g.test(newPassword);
+
+        return   (newPassword.length >= 8)
+        && hasNumbers && hasUpperCase;
+    }
+    
+}
+
+let user = new User("Jai", "Paasdds23")
+console.log(user.getPassword())
+
+user.setPassword("234")
+console.log(user.getPassword())
+
+user.setPassword("12Pasword")
+console.log(user.getPassword());
