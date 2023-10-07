7eee6124c808e77f59fa3b32f11008f57a48e6f1
Jai <jaicool4242@gmail.com>
2023-10-07 13:06:51 +0530

    JS program for multiplying numbers with hoisting is completed.

---
+++ Working of JavaScript/multiplyNumbers.js
@@ -0,0 +1,12 @@
+// JS Program for multiplying two numbers and displaying hositing concept //
+
+// CALLING FUNCTION BEFORE DECLARING ANONYMOUS FUNCTION WITH FUNCTION EXPRESSION. 
+console.log(multiplyNum(32,12));
+
+
+// DECLARING FUNCTION 
+let multiplyNum = function (a,b){
+    return c = a*b;
+}
+
+// OUTPUT : ERROR :- CANNOT ACCESS MULTIPLYNUM BEFORE INITIALIZATION
