6232378171639874d53227ccc8d53c731c48ed10
Jai <jaicool4242@gmail.com>
2023-10-07 13:55:17 +0530

    JS Program for checking temporal dead zone is completed

---
+++ Working of JavaScript/deadZone.js
@@ -0,0 +1,10 @@
+// JS Program for Testing Temporal Dead Zone //
+
+{
+    console.log(x);
+    // can't initialize before declaring the variable.
+
+    let x = " Jai Hind!";
+
+    console.log(x);// Jai Hind!
+}
