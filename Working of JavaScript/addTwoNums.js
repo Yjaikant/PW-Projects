76680d5307758dae1fad61d57374fcde743b7e8b
Jai <jaicool4242@gmail.com>
2023-10-07 13:13:53 +0530

    JS PRogram for adding two Numbers(hoisting) is completed.

---
+++ Working of JavaScript/addTwoNums.js
@@ -0,0 +1,11 @@
+// JS Program for adding two numbers and console before assigning //
+
+function addTwoNums(a,b){
+    console.log(c);// undfined
+    var c;// undefined
+     c = a+b;
+    return c;//38 will be returned
+
+}
+
+console.log(addTwoNums(4,34));// 38
