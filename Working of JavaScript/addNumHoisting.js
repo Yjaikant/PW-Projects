cbbd8c7d8bdf372da376ac11521e4dcc91f3be03
Jai <jaicool4242@gmail.com>
2023-10-07 12:57:30 +0530

    JS PRogram for Add Numbers with hoisting concept is completed.

---
+++ Working of JavaScript/addNumHoisting.js
@@ -0,0 +1,8 @@
+// JS Program for adding two numbers and hoisting //
+// CALLING FUNCTION BEFORE DECLARING 
+console.log(addNum(2,3))
+//DEFINING FUNCTION
+function addNum(a,b){
+    const c = a+b;
+    return c;
+}
